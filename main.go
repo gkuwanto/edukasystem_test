ec6f84a95312e40d22a575c7bf9a275e13e047f4
Garry Kuwanto <gkuwanto@gmail.com>
2019-04-29 22:22:59 +0700

    Implemented Super Sorting with bad sorting algo

--- main.go
+++ main.go
@@ -12,6 +12,10 @@ import (
 	"time"
 
 	"github.com/julienschmidt/httprouter"
+
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type data struct {
@@ -185,8 +189,55 @@ func moveUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 }
 
+type userScore struct {
+	ID    int     `json:"id_user"`
+	Score float64 `json:"score"`
+	Rank  int     `json:"rank"`
+}
+
+type users [5000]userScore
+
 func superSort(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "To be implemented")
+	db, err := sql.Open("mysql", "guest:codingtest@tcp(data.edukasystem.id:3306)/dummy")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	ranks := users{}
+
+	rows, queryErr := db.Query("SELECT id_user, score FROM Score")
+	if queryErr != nil {
+		log.Fatal(queryErr)
+	}
+	defer rows.Close()
+	numOfRows := 0
+	for rows.Next() {
+		scanErr := rows.Scan(&ranks[numOfRows].ID, &ranks[numOfRows].Score)
+		if scanErr != nil {
+			log.Fatal(scanErr)
+		}
+		ranks[numOfRows].Rank = numOfRows
+		numOfRows++
+	}
+	for i := 0; i < 5000; i++ {
+		for j := 0; j < 5000; j++ {
+			if ranks[i].Score > ranks[j].Score {
+				temp := ranks[i].Rank
+				ranks[i].Rank = ranks[j].Rank
+				ranks[j].Rank = temp
+
+				tem := ranks[i]
+				ranks[i] = ranks[j]
+				ranks[j] = tem
+			}
+		}
+	}
+	json.NewEncoder(w).Encode(ranks)
 }
 
 func main() {
