7498c44d73ad186246e4f4a8468b7b67793295b3
Garry Kuwanto <gkuwanto@gmail.com>
2019-04-29 21:09:27 +0700

    implemented random user

--- Magic Update/main.go
+++ Magic Update/main.go
@@ -1,14 +1,67 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
 
-func moveUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+type data struct {
+	Msg  string `json:"message"`
+	Data struct {
+		ID   int `json:"id_user"`
+		City int `json:"id_city"`
+	}
+}
 
+func generateUserData() data {
+	url := "https://api.edukasystem.id/dummy/user/"
+	data1 := data{}
+	id := 300000000
+	for data1.Msg != "ok" {
+		rand.Seed(time.Now().UnixNano())
+		id = 10000 + rand.Intn(10000)
+
+		edukaClient := http.Client{
+			Timeout: time.Second * 20,
+		}
+
+		req, err := http.NewRequest(http.MethodGet, url+strconv.Itoa(id), nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		req.Header.Set("User-Agent", "edukasystem-web-api-challange")
+
+		res, getErr := edukaClient.Do(req)
+		if getErr != nil {
+			log.Fatal(getErr)
+		}
+
+		body, readErr := ioutil.ReadAll(res.Body)
+		if readErr != nil {
+			log.Fatal(readErr)
+		}
+
+		jsonErr := json.Unmarshal(body, &data1)
+		if jsonErr != nil {
+			log.Fatal(jsonErr)
+		}
+		fmt.Println(data1.Msg)
+	}
+	return data1
+}
+
+func moveUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	data1 := generateUserData()
+
+	fmt.Println(data1.Msg)
 }
 
 func main() {
